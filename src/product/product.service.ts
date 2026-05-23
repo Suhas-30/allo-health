a6308ab039810487a1428051a7568a16c646bc17
Suhas-30 <3suhashs@gmail.com>
2026-05-23 13:18:53 +0530

    fix stock implicit any type

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,16 +1,19 @@
 import { productRepository } from "./product.repository"
 import { ProductResponse } from "@/src/types"
 
+type ProductWithStock = Awaited<ReturnType<typeof productRepository.findAllWithStock>>[0]
+type StockWithWarehouse = ProductWithStock["stocks"][0]
+
 export const productService = {
   getAllProducts: async (): Promise<ProductResponse[]> => {
     const products = await productRepository.findAllWithStock()
 
-    return products.map((product) => ({
+    return products.map((product: ProductWithStock) => ({
       id: product.id,
       name: product.name,
       description: product.description,
       price: Number(product.price),
-      warehouses: product.stocks.map((stock) => ({
+      warehouses: product.stocks.map((stock: StockWithWarehouse) => ({
         stockId: stock.id,
         warehouseId: stock.warehouseId,
         warehouseName: stock.warehouse.name,
