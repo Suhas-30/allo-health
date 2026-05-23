fdae946d652c1386e525e37051283fb880998544
Suhas-30 <3suhashs@gmail.com>
2026-05-23 12:48:11 +0530

    fix prisma type check fix fixed

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,13 @@
 import { PrismaNeon } from "@prisma/adapter-neon"
-import { PrismaClient } from "@prisma/client/edge"
+import Prisma from "@prisma/client"
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient }
+const globalForPrisma = global as unknown as { prisma: Prisma.PrismaClient }
 
 function createPrismaClient() {
   const adapter = new PrismaNeon({
     connectionString: process.env.DATABASE_URL!,
   })
-  return new PrismaClient({ adapter })
+  return new Prisma.PrismaClient({ adapter })
 }
 
 export const prisma = globalForPrisma.prisma || createPrismaClient()
