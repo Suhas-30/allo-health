b03725f33236e5b77498fcc8b4f80bc071d61f27
Suhas-30 <3suhashs@gmail.com>
2026-05-23 12:41:13 +0530

    fix prisma client import for v7

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,5 @@
-import { PrismaClient } from "@prisma/client"
 import { PrismaNeon } from "@prisma/adapter-neon"
+import { PrismaClient } from "@prisma/client/edge"
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
