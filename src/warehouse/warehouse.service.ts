7febbd3a0b9a5752fe0017407e959bf6d59debc1
Suhas-30 <3suhashs@gmail.com>
2026-05-23 13:22:13 +0530

    fix warehouse implicit any type

--- src/warehouse/warehouse.service.ts
+++ src/warehouse/warehouse.service.ts
@@ -1,11 +1,13 @@
 import { warehouseRepository } from "./warehouse.repository"
 import { WarehouseResponse } from "@/src/types"
 
+type WarehouseWithStock = Awaited<ReturnType<typeof warehouseRepository.findAll>>[0]
+
 export const warehouseService = {
   getAllWarehouses: async (): Promise<WarehouseResponse[]> => {
     const warehouses = await warehouseRepository.findAll()
 
-    return warehouses.map((warehouse) => ({
+    return warehouses.map((warehouse: WarehouseWithStock) => ({
       id: warehouse.id,
       name: warehouse.name,
       location: warehouse.location,
